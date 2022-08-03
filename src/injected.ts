f930bc17104eb8b0ef713354753814cf52c23003
Friedrich Völker <75260130+lan1Friedrich@users.noreply.github.com>
2022-08-03 17:02:39 +0200

    Git Stuff

--- src/injected.ts
+++ src/injected.ts
@@ -1382,7 +1382,6 @@ const injectDarkMode = () => {
             background-color: var(--color-dark);
             color: white;
         }
-<<<<<<< HEAD
         body.dark-mode .rcx-message-reactions__reaction {
             border-color: var(--color-dark);
         }
@@ -1392,8 +1391,6 @@ const injectDarkMode = () => {
         }
 
 
-=======
->>>>>>> 5845838b006db04ae54b705bf521c6a3a7f48f09
 
         body.dark-mode .rcx-message-toolbox {
             background-color: var(--color-dark-medium);
@@ -1571,8 +1568,4 @@ const injectDarkMode = () => {
         $(addDarkModeToggle);
   }
   start();
-<<<<<<< HEAD
   injectDarkMode()
-=======
-  injectDarkMode()
->>>>>>> 5845838b006db04ae54b705bf521c6a3a7f48f09
